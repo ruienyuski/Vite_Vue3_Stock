6f0b02c46142c7496793fc36755a77b86f40662c
ruienyuski <yuski.tw@gmail.com>
2024-02-04 23:25:56 +0800

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import Layout from './components/Layout.vue'
 </script>
 
 <template>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
   <Layout />
 </template>
 
